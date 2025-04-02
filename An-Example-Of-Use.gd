25e43abf49223eda9bd87e2ce74410c35886d042
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-04-02 20:38:57 -0300

    ReCorner Is Here

---
+++ An-Example-Of-Use.gd
@@ -0,0 +1,34 @@
+class_name ReCorner extends CharacterBody2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+@export var ReCorner_Snap_Range: int = 20
+
+func ReCorner(delta: float) -> void:
+    var move_vec = Vector2(0, velocity.y * delta)
+
+    if velocity.y >= 0 or not test_move(global_transform, move_vec):
+        return
+
+    for i in range(1, ReCorner_Snap_Range):
+        for dir in [-1, 1]:
+            if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), move_vec):
+                translate(Vector2(i / 2.0 * dir, 0))
+                if velocity.x * dir < 0:
+                    velocity.x = 0
+                return
+
+func _physics_process(delta: float) -> void:
+    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
+
+    var direction := Input.get_axis("ui_left", "ui_right")
+    if direction:
+        velocity.x = direction * SPEED
+    else:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+
+    ReCorner(delta)
+    velocity += get_gravity() * delta
+    move_and_slide()
