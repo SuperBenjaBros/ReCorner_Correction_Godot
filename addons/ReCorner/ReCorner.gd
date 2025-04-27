e70604e6dd7981f8ccc1bf2b74e7fb81f3524375
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-04-27 01:49:55 -0300

    No More Fake Erros
    In ReCorner.gd Godot Was Giving Error For Not Having Velocity, Add And Setup Section And Don't More...

--- addons/ReCorner/ReCorner.gd
+++ addons/ReCorner/ReCorner.gd
@@ -1,3 +1,10 @@
+# --- Setup Section ---
+# Don't Copy This, It Looks Nice Here, But It Will Probably Look Bad In Your Code
+
+class_name ReCorner extends CharacterBody2D
+
+# --- End Setup Section ---
+
 @export var ReCorner_Snap_Range: int = 20
 
 func ReCorner(delta: float) -> void:
