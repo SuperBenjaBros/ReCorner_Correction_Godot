91b544cbfbd3f12530647a9934dc5b6bf7fb1172
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-05-10 21:59:31 -0300

    Add Physic Interpolation Compatibility And Little Changues

--- addons/ReCorner/An-Example-Of-Use.gd
+++ addons/ReCorner/An-Example-Of-Use.gd
@@ -6,16 +6,14 @@ const JUMP_VELOCITY = -400.0
 @export var ReCorner_Snap_Range: int = 20
 
 func ReCorner(delta: float) -> void:
-
-	if velocity.y >= 0 or not test_move(global_transform, Vector2(0, velocity.y * delta)):
-		return
+	if velocity.y >= 0 or not test_move(global_transform, Vector2(0, velocity.y * delta)): return
 
 	for i in range(1, ReCorner_Snap_Range):
 		for dir in [-1, 1]:
 			if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), Vector2(0, velocity.y * delta)):
 				translate(Vector2(i / 2.0 * dir, 0))
-				if velocity.x != 0:
-					velocity.x = 0
+				reset_physics_interpolation()
+				if velocity.x != 0: velocity.x = 0
 				return
 
 func _physics_process(delta: float) -> void:
--- addons/ReCorner/ReCorner.gd
+++ addons/ReCorner/ReCorner.gd
@@ -5,14 +5,12 @@ class_name ReCorner extends CharacterBody2D
 @export var ReCorner_Snap_Range: int = 20
 
 func ReCorner(delta: float) -> void:
-
-	if velocity.y >= 0 or not test_move(global_transform, Vector2(0, velocity.y * delta)):
-		return
+	if velocity.y >= 0 or not test_move(global_transform, Vector2(0, velocity.y * delta)): return
 
 	for i in range(1, ReCorner_Snap_Range):
 		for dir in [-1, 1]:
 			if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), Vector2(0, velocity.y * delta)):
 				translate(Vector2(i / 2.0 * dir, 0))
-				if velocity.x != 0:
-					velocity.x = 0
+				reset_physics_interpolation()
+				if velocity.x != 0: velocity.x = 0
 				return
